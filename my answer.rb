0a57006834ea8088e4abcc7ce7b6bddeffb38057
Kajiwara-Yudai <89145357+Kajiwara-Yudai@users.noreply.github.com>
2021-08-24 22:46:47 +0900

    Update my answer.rb

--- my answer.rb
+++ my answer.rb
@@ -1,16 +1,7 @@
-def fibonacci
-    a = 0
-    b = 1
-    loop do
-        a, b = b, a + b
-        yield a
-    end
+def fibonacci4(n)
+  return   if n < 0
+  return n if n < 2
+  a, b = 0, 1
+  n.times { a, b = b, a + b }
+  a
 end
-
-n = 1
-fibonacci do |i|
-break if 100 < a
-    puts "#{n} #{i}"
-    n +=1
-end
-
