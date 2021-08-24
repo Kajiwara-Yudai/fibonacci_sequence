6191b4254d1faf970ea3907bb6cdc1fbb0228567
Kajiwara-Yudai <89145357+Kajiwara-Yudai@users.noreply.github.com>
2021-08-24 22:55:43 +0900

    Update my answer.rb

--- my answer.rb
+++ my answer.rb
@@ -1,24 +1,11 @@
-
-require 'benchmark'
-
-n = 35
-Benchmark.bm(7) do |x|
-  x.report("memo") {
-    # 修正前(メモがメソッドの定義内なので効果なし)
-    def fibonacci(input)
-      memo = { 1=> 1, 2=> 1 }
-      return memo[input] if memo.has_key?(input)
-      memo[input] = fibonacci(input - 2) + fibonacci(input - 1)
-    end
-    fibonacci(n)
-  }
-  x.report("@memo") {
-    # 修正後(メモがメソッドのスコープ外なので効果あり)
-    @memo = { 1=> 1, 2=> 1 }
-    def fibonacci(input)
-      return @memo[input] if @memo
-      @memo[input] = fibonacci(input - 2) + fibonacci(input - 1)
-    end
-    fibonacci(n)
-  }
+class FibonacciSequence
+  def self.fibonacci_item(n)
+    if n == 0
+      return 0
+    elsif n == 1
+      return 1
+    else
+      fibonacci_item(n - 1) + fibonacci_item(n - 2)
+      end
+  end
 end
