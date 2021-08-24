daa6b76eb8196976d2b4cf8d9f33f1284eaeee1e
Kajiwara-Yudai <89145357+Kajiwara-Yudai@users.noreply.github.com>
2021-08-24 22:43:47 +0900

    Update my answer.rb

--- my answer.rb
+++ my answer.rb
@@ -1,9 +1,16 @@
-a = 0
-b = 1
-loop do 
-    tmp = a
-    a, b = b, a + b
-    break if 100 < a
-    puts a
+def fibonacci
+    a = 0
+    b = 1
+    loop do
+        a, b = b, a + b
+        yield a
+    end
+end
+
+n = 1
+fibonacci do |i|
+break if 100 < a
+    puts "#{n} #{i}"
+    n +=1
 end
 
