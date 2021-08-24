92c934b14bdf75dbd485ffaf9be9c46b48d7a3ba
Kajiwara-Yudai <89145357+Kajiwara-Yudai@users.noreply.github.com>
2021-08-24 22:49:53 +0900

    Update my answer.rb

--- my answer.rb
+++ my answer.rb
@@ -1,7 +1,24 @@
-def fibonacci4(n)
-  return   if n < 0
-  return n if n < 2
-  a, b = 0, 1
-  n.times { a, b = b, a + b }
-  a
+
+require 'benchmark'
+
+n = 35
+Benchmark.bm(7) do |x|
+  x.report("memo") {
+    # 修正前(メモがメソッドの定義内なので効果なし)
+    def fibonacci(input)
+      memo = { 1=> 1, 2=> 1 }
+      return memo[input] if memo.has_key?(input)
+      memo[input] = fibonacci(input - 2) + fibonacci(input - 1)
+    end
+    fibonacci(n)
+  }
+  x.report("@memo") {
+    # 修正後(メモがメソッドのスコープ外なので効果あり)
+    @memo = { 1=> 1, 2=> 1 }
+    def fibonacci(input)
+      return @memo[input] if @memo
+      @memo[input] = fibonacci(input - 2) + fibonacci(input - 1)
+    end
+    fibonacci(n)
+  }
 end
